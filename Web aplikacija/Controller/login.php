3ab05b9c7d1844c677c527803ce8688fe237c636
tcundic <tcundic@gmail.com>
2014-11-22 17:54:10 +0100

    login3

--- Web aplikacija/Controller/login.php
+++ Web aplikacija/Controller/login.php
@@ -4,11 +4,6 @@ require_once("base.php");
 
 session_start();
 
-if (isset($_SESSION['logged']) && $_SESSION['logged'] == true) {
-    header("Location: " . dirname($_SERVER['REQUEST_URI']) . '/index.php');
-    exit();
-}
-
 $base = new base();
 
 if(isset($_POST['submit'])) {
