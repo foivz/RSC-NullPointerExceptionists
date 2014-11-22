2eb9f4ed817aa7d8ea7ae3a627638ee4bfea968b
Danijel Filipović <danijel.filipovic@live.com>
2014-11-22 21:01:08 +0100

    Ponovljeni komit 2
    Ponovljeni komit 2

--- Web aplikacija/VIew/registracija.php
+++ Web aplikacija/VIew/registracija.php
@@ -5,43 +5,68 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="CSS/style-index.css">
+    <link rel="stylesheet" type="text/css" href="CSS/style-form.css">
 </head>
 <body>
 
-<form name="login" id="login" action="../Controller/registracija.php" method="POST">
-    <div class="labels" style="float: left;">
-        <label for="name">Ime:</label><br />
-        <label for="surname">Prezime:</label><br />
-        <label for="email">E-mail:</label><br />
-        <label for="username">Korisničko ime:</label><br />
-        <label for="password1">Lozinka:</label><br />
-        <label for="password2">Potvrdi lozinku:</label><br />
-        <label for="date_of_birth">Datum rođenja:</label><br />
-        <label for="weight">Težina</label><br />
-        Izaberite spol:<br/>
-        <input type="radio" name="gender"> Muški<br/>
-        <input type="radio" name="gender"> Ženski<br/>
-        <label for="city">Grad</label><br />
-        <label for="oib">OIB</label><br />
-        <label for="blood_type">Krvna grupa</label><br />
-        <label for="institution">Institucija</label><br />
+<form name="login" id="login" class="form" action="../Controller/registracija.php" method="POST">
+    <div class="input-label">
+        <label for="name">Ime:</label>
+        <input class="input" type="text" id="name" name="name" size="30" maxlength="15" placeholder="Upiši ime"><span id="nameError" class="error" ></span>
     </div>
-    <div class="registration" style="float: left;">
-        <input class="input" type="text" id="name" name="name" size="30" maxlength="15" placeholder="Upiši ime"><span id="nameError" class="error" ></span><br/>
-        <input class="input" type="text" id="surname" name="surname" size="30" maxlength="30" placeholder="Upiši prezime"><span id="surnameError" class="error"></span><br/>
+    <div class="input-label">
+        <label for="surname">Prezime:</label>
+        <input class="input" type="text" id="surname" name="surname" size="30" maxlength="30" placeholder="Upiši prezime"><span id="surnameError" class="error"></span>
+    </div>
+    <div class="input-label">
+        <label for="email">E-mail:</label>
         <input class="input" type="email" id="email" name="email" size="30" maxlength="30" placeholder="Napiši email adresu"><span id="emailError" class="error"></span><br>
-        <input class="input" type="text" id="username" name="username" size="30" maxlength="30" placeholder="Upiši korisničko ime"><span id="usernameError" class="error"></span><br/>
-        <input class="input" type="password" name="password1" id="password1" size="30" maxlength="20" placeholder="Barem 6 znakova"><br>
-        <input class="input" type="password" name="password2" id="password2" size="30" maxlength="20" placeholder="Ponovi lozinku"><br>
-        <input class="input" type="date" name="date_of_birth" placeholder="DD.MM.GGGG" id="date_of_birth"><br /><br />
-        <input class="input" type="number" id="weight" name="weight" size="30" maxlength="15" placeholder="Upiši težinu"><br/>
-        <input class="input" type="text" id="city" name="city" size="30" maxlength="15" placeholder="Upiši grad"><br/>
-        <input class="input" type="number" id="oib" name="oib" placeholder="Upiši OIB"><br/>
-        <input class="input" type="number" id="blood_type" name="blood_type" placeholder="Upiši krvnu grupu"><br/>
-        <input class="input" type="text" id="institution" name="institution" placeholder="Upiši ime institucije"><br/>
-        <br /><input class="Button" name="submit" type="submit" value="Registriraj se">
     </div>
+    <div class="input-label">
+        <label for="username">Korisničko ime:</label>
+        <input class="input" type="text" id="username" name="username" size="30" maxlength="30" placeholder="Upiši korisničko ime"><span id="usernameError" class="error"></span>
+    </div>
+    <div class="input-label">
+        <label for="password1">Lozinka:</label>
+        <input class="input" type="password" name="password1" id="password1" size="30" maxlength="20" placeholder="Barem 6 znakova"><br>
+    </div>
+    <div class="input-label">
+        <label for="password2">Potvrdi lozinku:</label>
+        <input class="input" type="password" name="password2" id="password2" size="30" maxlength="20" placeholder="Ponovi lozinku"><br>
+    </div>
+    <div class="input-label">
+        <label for="date_of_birth">Datum rođenja:</label>
+        <input class="input" type="date" name="date_of_birth" placeholder="DD.MM.GGGG" id="date_of_birth">
+    </div>
+    <div class="input-label">
+        <label for="weight">Težina</label>
+        <input class="input" type="number" id="weight" name="weight" size="30" maxlength="15" placeholder="Upiši težinu">
+    </div>
+    <div class="input-label">
+        <div class="faux-label">Izaberite spol:</div>
+        <input type="radio" name="gender"> Muški
+        <input type="radio" name="gender"> Ženski
+    </div>
+    <div class="input-label">
+        <label for="city">Grad</label>
+        <input class="input" type="text" id="city" name="city" size="30" maxlength="15" placeholder="Upiši grad">
+    </div>
+    <div class="input-label">
+        <label for="oib">OIB</label>
+        <input class="input" type="number" id="oib" name="oib" placeholder="Upiši OIB">
+    </div>
+    <div class="input-label">
+        <label for="blood_type">Krvna grupa</label>
+        <input class="input" type="number" id="blood_type" name="blood_type" placeholder="Upiši krvnu grupu">
+    </div>
+    <div class="input-label">
+        <label for="institution">Institucija</label>
+        <input class="input" type="text" id="institution" name="institution" placeholder="Upiši ime institucije">
+    </div>
+
+    <div class="input-label">
+        <input class="Button" name="submit" type="submit" value="Registriraj se">
+        </div>
 </form>
 
 </body>
