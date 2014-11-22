7183ace79c8dd9a92f2fef5b31b96cb4fae5cc37
Danijel Filipović <danijel.filipovic@live.com>
2014-11-22 16:04:38 +0100

    Napravljen početni index.php
    Napravljen je početni index.php i dodana je mapa Model.

---
+++ Web aplikacija/Model/Institution.php
@@ -0,0 +1,8 @@
+<?php
+
+	class Institution
+	{
+		private $id;
+	}
+
+?>
---
+++ Web aplikacija/index.php
@@ -0,0 +1,39 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="View/CSS/style-index.css">
+    </head>
+    <body>
+        <header>
+            <div id="banner">
+            </div>
+
+            <nav id="navigation">
+                <ul>
+                    <li>
+                        <a href="#">Početna</a>
+                    </li> 
+                    <li>
+                        <a href="#">Nešto</a>
+                    </li>
+                    <li>
+                        <a href="#">Odjava</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+
+        <div id="iframe-container">
+                <iframe src="" frameborder="0">
+                    Došlo je do greške kod učitavnja stranice.
+                </iframe>
+        </div>
+
+        <footer>
+        </footer>
+    </body>
+</html>
